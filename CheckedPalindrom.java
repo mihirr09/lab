8cdfbfa53e19b4b71f88c4a48e6a72b520f9f1b2
mihirr09 <109658159+mihirr09@users.noreply.github.com>
2022-08-19 13:33:32 +0530

    Add files via upload

---
+++ CheckedPalindrom.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class CheckedPalindrom {
+
+	  public static boolean isPalindrome(String str) {
+		    int i = 0, j = str.length() - 1;
+		    while (i < j) {
+		      if (str.charAt(i) != str.charAt(j)) {
+		        return false;
+		      }
+		      i++;
+		      j--;
+		    }
+		    return true;
+		  }
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Enter a string");
+    String str = scn.next();
+     boolean flag =isPalindrome(str);
+     if(flag == true) {
+    	 System.out.println("String is palindrom");
+     }else {
+    	 System.out.println("String is not  palindrom");
+     }
+    scn.close();
+  }
+
+}
